2dc78f8c5c4b45c10031475178c7deff6fbe4529
Daeeun Ko <7273322@naver.com>
2023-08-28 03:48:58 +0900

    자바빈 User 클래스 생성
    자바빈 관례
    1) 디폴트 생성자: 리플렉션으로 오브젝트를 생성해야 하므로 디폴트 생성자가 필요하다. 자바는 생성자가 없을 경우 디폴트 생성자가 자동으로 생성되지만 명시적으로 @NoArgsConstructor를 작성해주었다.
    2) 프로퍼티: 자바빈이 노출하는 이름을 가진 속성을 프로퍼티라 한다. 자바빈은 이를 수정하고 조회하기 위해 수정자 메서드와 접근자 메서드를 제공해야 한다. @Getter와 @Setter로 해당 메서드를 제공했다.
    
    1.1 초난감 DAO

---
+++ src/main/java/study/spring/tobispring/user/domain/User.java
@@ -0,0 +1,14 @@
+package study.spring.tobispring.user.domain;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+public class User {
+    String id;
+    String name;
+    String  password;
+}
