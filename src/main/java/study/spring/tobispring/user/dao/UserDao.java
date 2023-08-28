36eee6cc4932e4b7d13a80358d0ad4460aa77afc
Daeeun Ko <7273322@naver.com>
2023-08-28 12:48:20 +0900

    커넥션을 가져오는 코드 메서드 추출
    1.2 DAO의 분리

--- src/main/java/study/spring/tobispring/user/dao/UserDao.java
+++ src/main/java/study/spring/tobispring/user/dao/UserDao.java
@@ -7,11 +7,7 @@ import java.sql.*;
 public class UserDao {
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("org.h2.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:h2:tcp://localhost/~/H2DB/tobi;MODE=MYSQL",
-                "sa",
-                "1234");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?, ?, ?)");
         ps.setString(1, user.getId());
@@ -25,11 +21,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("org.h2.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:h2:tcp://localhost/~/H2DB/tobi;MODE=MYSQL",
-                "sa",
-                "1234");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
@@ -46,5 +38,15 @@ public class UserDao {
         c.close();
 
         return user;
+    }
+
+
+    private static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:h2:tcp://localhost/~/H2DB/tobi;MODE=MYSQL",
+                "sa",
+                "1234");
+        return c;
     }
 }
